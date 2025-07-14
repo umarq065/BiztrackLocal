ac239208e2126ca0da0df8185668ff126a13fe77
Fiction Animated <fictionanimated@gmail.com>
2025-07-14 06:16:09 +0000

    the connection page said
    Database Connection Test
    This page tests the

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -6,7 +6,9 @@ if (!process.env.MONGODB_URI) {
 }
 
 const uri = process.env.MONGODB_URI;
-const options = {};
+const options = {
+  tlsDisableOCSPEndpointCheck: true,
+};
 
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
